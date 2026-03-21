235a882d1e2e6122a6cfc379d7ee35bea45ecb1d
Merten Lampe <mertenlampe@Mertens-MacBook.local>
2026-03-21 21:17:19 +0100

    Add webui embed stubs and MCP2515 fixes

---
+++ src/webui_embed_stubs.cpp
@@ -0,0 +1,16 @@
+extern "C" {
+const unsigned char _binary_index_html_gz_start[] = {0};
+const unsigned char _binary_index_html_gz_end[]   = {0};
+
+const unsigned char _binary_main_js_gz_start[] = {0};
+const unsigned char _binary_main_js_gz_end[]   = {0};
+
+const unsigned char _binary_main_css_gz_start[] = {0};
+const unsigned char _binary_main_css_gz_end[]   = {0};
+
+const unsigned char _binary_pico_min_css_gz_start[] = {0};
+const unsigned char _binary_pico_min_css_gz_end[]   = {0};
+
+const unsigned char _binary_md5_min_js_gz_start[] = {0};
+const unsigned char _binary_md5_min_js_gz_end[]   = {0};
+}
