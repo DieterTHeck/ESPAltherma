c55bb0d1d3115c16cd52784bd237837dcc26aa81
Merten Lampe <mertenlampe@MertenMacBookAir.lan>
2026-03-21 21:34:20 +0100

    Add webui embed stubs (uint8_t symbols with stdint include)

--- src/webui_embed_stubs.cpp
+++ src/webui_embed_stubs.cpp
@@ -1,16 +1,17 @@
+#include <stdint.h>
 extern "C" {
-const uint8_t _binary_index_html_gz_start[] = {0};
-const uint8_t _binary_index_html_gz_end[]   = {0};
+const uint8_t _binary_index_html_gz_start[] asm("_binary_index_html_gz_start") = {0};
+const uint8_t _binary_index_html_gz_end[]   asm("_binary_index_html_gz_end")   = {0};
 
-const uint8_t _binary_main_js_gz_start[] = {0};
-const uint8_t _binary_main_js_gz_end[]   = {0};
+const uint8_t _binary_main_js_gz_start[] asm("_binary_main_js_gz_start") = {0};
+const uint8_t _binary_main_js_gz_end[]   asm("_binary_main_js_gz_end")   = {0};
 
-const uint8_t _binary_main_css_gz_start[] = {0};
-const uint8_t _binary_main_css_gz_end[]   = {0};
+const uint8_t _binary_main_css_gz_start[] asm("_binary_main_css_gz_start") = {0};
+const uint8_t _binary_main_css_gz_end[]   asm("_binary_main_css_gz_end")   = {0};
 
-const uint8_t _binary_pico_min_css_gz_start[] = {0};
-const uint8_t _binary_pico_min_css_gz_end[]   = {0};
+const uint8_t _binary_pico_min_css_gz_start[] asm("_binary_pico_min_css_gz_start") = {0};
+const uint8_t _binary_pico_min_css_gz_end[]   asm("_binary_pico_min_css_gz_end")   = {0};
 
-const uint8_t _binary_md5_min_js_gz_start[] = {0};
-const uint8_t _binary_md5_min_js_gz_end[]   = {0};
+const uint8_t _binary_md5_min_js_gz_start[] asm("_binary_md5_min_js_gz_start") = {0};
+const uint8_t _binary_md5_min_js_gz_end[]   asm("_binary_md5_min_js_gz_end")   = {0};
 }
