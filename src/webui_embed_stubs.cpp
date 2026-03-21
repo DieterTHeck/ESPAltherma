99df20078bc9cbef8393dc031c22fb80e5adced8
Merten Lampe <mertenlampe@MertenMacBookAir.lan>
2026-03-21 21:33:41 +0100

    Add webui embed stubs (uint8_t symbols) for LittleFS mode

--- src/webui_embed_stubs.cpp
+++ src/webui_embed_stubs.cpp
@@ -1,16 +1,16 @@
 extern "C" {
-const unsigned char _binary_index_html_gz_start[] asm("_binary_index_html_gz_start") = {0};
-const unsigned char _binary_index_html_gz_end[]   asm("_binary_index_html_gz_end")   = {0};
+const uint8_t _binary_index_html_gz_start[] = {0};
+const uint8_t _binary_index_html_gz_end[]   = {0};
 
-const unsigned char _binary_main_js_gz_start[] asm("_binary_main_js_gz_start") = {0};
-const unsigned char _binary_main_js_gz_end[]   asm("_binary_main_js_gz_end")   = {0};
+const uint8_t _binary_main_js_gz_start[] = {0};
+const uint8_t _binary_main_js_gz_end[]   = {0};
 
-const unsigned char _binary_main_css_gz_start[] asm("_binary_main_css_gz_start") = {0};
-const unsigned char _binary_main_css_gz_end[]   asm("_binary_main_css_gz_end")   = {0};
+const uint8_t _binary_main_css_gz_start[] = {0};
+const uint8_t _binary_main_css_gz_end[]   = {0};
 
-const unsigned char _binary_pico_min_css_gz_start[] asm("_binary_pico_min_css_gz_start") = {0};
-const unsigned char _binary_pico_min_css_gz_end[]   asm("_binary_pico_min_css_gz_end")   = {0};
+const uint8_t _binary_pico_min_css_gz_start[] = {0};
+const uint8_t _binary_pico_min_css_gz_end[]   = {0};
 
-const unsigned char _binary_md5_min_js_gz_start[] asm("_binary_md5_min_js_gz_start") = {0};
-const unsigned char _binary_md5_min_js_gz_end[]   asm("_binary_md5_min_js_gz_end")   = {0};
+const uint8_t _binary_md5_min_js_gz_start[] = {0};
+const uint8_t _binary_md5_min_js_gz_end[]   = {0};
 }
